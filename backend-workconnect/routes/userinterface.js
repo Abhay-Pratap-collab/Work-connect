448387d3d850ddadf78a2b6c88ca2ff29ca865a2
Abhay Pratap <pratapabhay1810@gmail.com>
2026-06-06 12:01:33 +0530

    change in package.json

--- backend-workconnect/routes/userinterface.js
+++ backend-workconnect/routes/userinterface.js
@@ -16,21 +16,18 @@ router.get("/fetch_all_category", function (req, res) {
 
    })
 })
-router.post("/fetch_all_subcategory_by_categoryid",function(req,res)
-{
-   pool.query("select * from subcategory where categoryid=?",[req.body.categoryid],function(error,result)
-{
-   if(error)
-   {
-      res.status(500).json({status:false,mesage:'error'})
-   }
-   else{
-         res.status(200).json({status:true,data:result})
-   }
+router.post("/fetch_all_subcategory_by_categoryid", function (req, res) {
+   pool.query("select * from subcategory where categoryid=?", [req.body.categoryid], function (error, result) {
+      if (error) {
+         res.status(500).json({ status: false, mesage: 'error' })
+      }
+      else {
+         res.status(200).json({ status: true, data: result })
+      }
+   })
 })
-})
-router.post("/fetch_all_pricing",function(req,res){
-   pool.query("select pricing.*,subcategory.* from pricing,subcategory where pricing.subcategoryid=subcategory.subcategoryid and pricing.subcategoryid=?" ,[req.body.subcategoryid], function (error, result) {
+router.post("/fetch_all_pricing", function (req, res) {
+   pool.query("select pricing.*,subcategory.* from pricing,subcategory where pricing.subcategoryid=subcategory.subcategoryid and pricing.subcategoryid=?", [req.body.subcategoryid], function (error, result) {
       if (error) {
          console.log("xxxxx", error)
          res.status(500).json({ status: false, message: 'Pls Contact to DBA...' })
